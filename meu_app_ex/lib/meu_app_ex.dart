a4b1c279f274f738df45a57833cd5d813055d672
Lys <129811788+GioLys@users.noreply.github.com>
2023-09-28 16:59:46 -0300

    Add files via upload

---
+++ meu_app_ex/lib/meu_app_ex.dart
@@ -0,0 +1,39 @@
+
+import 'dart:io';
+
+import 'package:meu_app_ex/exception/nome_invalido_exception.dart';
+import 'package:meu_app_ex/models/Aluno.dart';
+import 'package:meu_app_ex/models/console_utils.dart';
+
+void execute(){
+  print("Bem vindo ao sistema de notas!");
+  String nome = consoleUtils.lerStringComTexto("Digite o nome do aluno:");
+  
+  try {
+    if (nome.trim() == ""){
+    throw NomeInvalidoException();
+  }
+  } on NomeInvalidoException {
+    print(NomeInvalidoException);
+    exit(0);
+  } catch (e) {
+    print(e);
+    exit(0);
+  }
+  
+  var aluno = Aluno(nome);
+  double? nota;
+  do {
+    nota = consoleUtils.lerDoubleComTextoComSaida("Digite a nota ou S para sair", "S");
+    if (nota != null){
+      aluno.adicionarNota(nota);
+    }
+  } while (nota != null);
+  print(aluno.getNotas());
+  print("A média do aluno foi: ${aluno.retornaMedia()}");
+  if (aluno.aprovado(7)){
+    print("O aluno ${aluno.getNome()} foi aprovado");
+  } else {
+    print("O aluno ${aluno.getNome()} foi reprovado");
+  }
+}
